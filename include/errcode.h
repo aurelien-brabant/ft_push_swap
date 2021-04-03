513c4c982c88c828e1f8686126f9461e9bdab4df
abrabant <abrabant@student.42.fr>
2021-04-03 11:29:16 +0200

    Add errcode.h

---
+++ include/errcode.h
@@ -0,0 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   errcode.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abrabant <abrabant@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/03 11:16:07 by abrabant          #+#    #+#             */
+/*   Updated: 2021/04/03 11:16:59 by abrabant         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ERRCODE_H
+# define ERRCODE_H
+# define ERRCODE_INVALID_NB 1
+# define ERRCODE_UNSUPPORTED_INSTRUCTION 2
+#endif
